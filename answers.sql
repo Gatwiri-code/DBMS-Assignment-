b2d51d287939417eaafcab7f8574817854ec7b67
Gatwiri-code <gatwiritrecy049@gmail.com>
2025-08-06 21:38:04 +0300

    Initial commit of database assignment

---
+++ answers.sql
@@ -0,0 +1,10 @@
+-- Assignment: Introduction to Databases
+
+-- Question 1: Write an SQL query to create a new database called salesDB.
+-- The CREATE DATABASE statement is used to create a new SQL database.
+CREATE DATABASE salesDB;
+
+-- Question 2: Write an SQL query to drop (delete) the database called demo.
+-- The DROP DATABASE statement is used to delete an existing SQL database.
+-- Be careful with this command, as it permanently deletes the database and all its data!
+DROP DATABASE demo;
